b5cc6a258da216d4dbd2649e409ada3af68a4e4c
Csikós Attila <zaeta17@gmail.com>
2020-04-20 20:29:04 +0200

    AsyncTask

---
+++ AsyncTask.h
@@ -0,0 +1,202 @@
+// Required STD headers
+// #include <exception>
+// #include <future>
+// #include <atomic>
+
+
+class IllegalStateException : std::exception
+{
+public:
+  enum class eEx : int { TaskIsAlreadyRunning, TaskIsAlreadyFinished };
+
+  eEx e;
+
+  IllegalStateException() = default;
+  IllegalStateException(eEx e) : e(e) {}
+};
+
+
+// AsyncTask 
+// Asynchronous task progress handler class
+//  - Asynchronous worker task should be defined into the doInBackground(), 
+//  - Feedback system elements should be handled by the onPreExecute()/onProgressUpdate()/onPostExecute()
+//  - Parametrized Ctor() or execute() start the doInBackground()
+//  - Refresh the feedback by the onCallbackLoop()
+// Nocopy object. onCallbackLoop() and get() could rethrow the doInBackground() thrown exceptions.
+template<typename Progress, typename Result, typename... Params>
+class AsyncTask
+{
+public:
+  enum class Status : int
+  {
+    PENDING, // Indicates that the task has not been executed yet.
+    RUNNING, // Indicates that the task is running.
+    FINISHED // Indicates that the task is finihsed: Cancelled, Exception was thrown or future object and AsyncTask::onPostExecute has finished.
+  };
+
+private:
+  Status mStatus = Status::PENDING;
+  Result mResult{};
+  std::future<Result> mFuture{}; // Future is a non-copyable object so AsyncTask also.
+  std::atomic<Progress> atProgress{};
+  std::atomic_bool atCancelled{};
+
+public:
+  AsyncTask() = default;
+  explicit AsyncTask(Params const&... params) { execute(params); }
+  
+protected:
+  AsyncTask(AsyncTask const&) = delete;
+  AsyncTask(AsyncTask&&) = delete;
+  AsyncTask operator=(AsyncTask const&) = delete;
+  AsyncTask operator=(AsyncTask&&) = delete;
+  virtual ~AsyncTask() 
+  {
+    auto const status = getStatus();
+    if (status != Status::RUNNING)
+      return;
+
+    if (!isCancelled())
+      cancel();
+
+    if (mFuture.valid())
+      finish(mFuture.get());
+  };
+
+public:
+
+  // Initiate the asynchronous task
+  // If the task is already began, IllegalStateException will be thrown
+  // @MainThread
+  AsyncTask<Progress, Result, Params...>& execute(Params const&... params)
+  {
+    switch (mStatus)
+    {
+      case Status::PENDING: break; // Everything is ok.
+      case Status::RUNNING: throw IllegalStateException(IllegalStateException::eEx::TaskIsAlreadyRunning);
+      case Status::FINISHED: throw IllegalStateException(IllegalStateException::eEx::TaskIsAlreadyFinished);
+    }
+
+    mStatus = Status::RUNNING;
+
+    onPreExecute();
+    mFuture = std::async
+    (
+      std::launch::async,
+      [this](Params const&... params) { return postResult(doInBackground(params...)); }, 
+      params...
+    );
+
+    return *this;
+  }
+
+  // @MainThread
+  Status getStatus() const { return mStatus; }
+
+protected:
+
+  // Background worker task
+  // Exception can be thrown, future object will rethrow it in get(), onCallbackLoop() or Dtor()
+  // @WorkerThread
+  virtual Result doInBackground(Params const&... params) = 0;
+
+  // Post process result if it is needed, still on the worker thread
+  // @WorkerThread
+  virtual Result postResult(Result&& result) noexcept { return result; }
+
+  // Usually to setup the feedback system
+  // @MainThread
+  virtual void onPreExecute() {}
+
+  // Usually to declare the finishing in the feedback system
+  // @MainThread
+  virtual void onPostExecute(Result const& result) {}
+
+  // Show progress in the feedback system
+  // @MainThread
+  virtual void onProgressUpdate(Progress const& values) {}
+
+  // Store the current state of the progress inside the class
+  // Use inside the doInBackground()
+  // @WorkerThread
+  virtual void publishProgress(Progress const& progress)
+  {
+    if (isCancelled())
+      return;
+
+    atProgress.store(progress);
+  }
+
+  // Cleanup function if the task is cancelled
+  //@MainThread
+  virtual void onCancelled() {}
+
+  // Cleanup function if the task is cancelled
+  //@MainThread
+  virtual void onCancelled(Result const& result) { onCancelled(); }
+
+public:
+  // Returns true if the task is cancelled by the cancel()
+  // It is usable to break process inside the doInBackground()
+  //@MainThread and @Workerthread
+  bool isCancelled() const { return atCancelled.load(); }
+
+  // Cancel the task
+  //@MainThread
+  void cancel() { atCancelled.store(true); }
+
+  // Get the result.
+  // It could freeze the mainthread if it invoked before the task is finished. Exception from the doInBackground can be rethrown.
+  //@MainThread
+  Result get()
+  {
+    if (getStatus() != Status::FINISHED)
+      finish(mFuture.get());
+
+    return mResult;
+  }
+
+  // Callback loop to refresh progress in the feedback system
+  // Return true if the task is finished. Exception from the doInBackground can be rethrown.
+  // @MainThread
+  bool onCallbackLoop()
+  {
+    if (mStatus == Status::PENDING || !mFuture.valid())
+      return false;
+
+    if (mStatus == Status::FINISHED)
+      return true;
+
+    auto const statusThread = mFuture.wait_for(std::chrono::seconds(0));
+
+    switch (statusThread)
+    {
+      case std::future_status::deferred:
+        return false;
+
+      case std::future_status::timeout:
+        onProgressUpdate(atProgress.load());
+        return false;
+
+      case std::future_status::ready:
+        finish(mFuture.get());
+        return true;
+
+      default:
+        return false;
+    }
+  }
+
+private:
+  // @MainThread
+  void finish(Result&& result)
+  {
+    mResult = result;
+    if (isCancelled())
+      onCancelled(mResult);
+    else
+      onPostExecute(mResult);
+
+    mStatus = Status::FINISHED;
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# AsyncTask
+# C++ AsyncTask.h
+AsyncTask is a C++ implementation of asynchronus task handling, including cancellation and progress feedback.
+The class interface is based on the similarly named Android java class.
+
+# Requirements
+Language standard: C++14 or above
+STD Headers: <exception>, <future>, <atomic>
+
+# Usage
+* Asynchronous worker task should be defined by overriding doInBackground()
+* Check the interruption of the task with the isCacelled() in the doInBackground()
+* Current progress can be stored using publishProgress() by doInBackground()
+* Feedback system elements should be handled by the onPreExecute()/onProgressUpdate()/onPostExecute()
+* Parametrized Ctor() or execute() starts the doInBackground()
+* Refresh the feedback repeatedly by the onCallbackLoop()
+* get() returns the Result of doInBackground()
+
+# Notes
+Header only implementation.
+Non-copyable object. Instance can be used only once. If the reusages needed, handle by a smart ptr (e.g.: std::unique_ptr<AsyncTaskChild> and std::unique_ptr::reset()).
+onCallbackLoop(), get(), ~AsyncTask() could rethrow the doInBackground() thrown exception. It is based on std::future object.
+If the AsyncTask is destructed while background task is running, ~AsyncTask() will execute the cancellation logical branch. This could cause temporary freeze until doInBackground() finish, or other troubles if the onPostExecute()'s resources are invalid.
+User interface, logsystem and other servicies can be attached by dependency injection.
+
